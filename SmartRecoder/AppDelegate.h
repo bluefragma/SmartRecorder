0f66b2ec7cf3ec4a11d82c61dccc283e3b1c283f
bluefragma <dkyang73@gmail.com>
2014-06-28 21:55:17 +0900

    Initial Commit

---
+++ SmartRecoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartRecoder
+//
+//  Created by 양동길 on 2014. 6. 28..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartRecoder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartRecoder
+//
+//  Created by 양동길 on 2014. 6. 28..
+//  Copyright (c) 2014년 양동길. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
