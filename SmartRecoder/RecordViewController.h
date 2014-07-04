30fcb6e0e8baedcf0bbb94a5bf8c70232ec22c2f
bluefragma <dkyang73@gmail.com>
2014-07-05 08:37:57 +0900

    259page

--- SmartRecoder/RecordViewController.h
+++ SmartRecoder/RecordViewController.h
@@ -7,8 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <AVFounda>
 
-@interface RecordViewController : UIViewController <AVAudioRecorderDelegate>
+@interface RecordViewController : UIViewController
 
 @end
